0e7d97abef4e8f472f42be6f70a835a79abc5768
Dipesh Thapa <dipesh@naphix.com.au>
2022-07-05 14:33:26 +0545

    DAta fetch using SWR

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import useSWR from 'swr'
+import DOMPurify  from 'dompurify'
 
-const footer = () => {
+const fetcher = (url) => fetch(url).then((res) => res.json())
+const COMMENTS_URL = 'http://frontity.test/wp-json/wp/v2/widgets?sidebar=wp-menu'
+
+
+const CommentsList = (props) => {
+  const { data, error } = useSWR(COMMENTS_URL, fetcher)
+  // because of the api, data will come in like: { comments: [{ content: ""}, ...] }
+
+  if (error) return <div>Something went wrong...</div>
+  if (!data) return <div>Loading...</div>
   return (
-    <div>footer</div>
+    <div className='upperFooter'>
+      <div className='container'>
+        <div className='upper-footer-row'>
+          <footer role="list">
+            <h2>Footer</h2>
+            {data && data.map((comment) => (
+                <div className="col-md-3" key={comment.id}>
+                  { <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(comment.rendered) }} /> }
+                </div>
+              ))}
+          </footer>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default footer
+export default CommentsList
--- pages/components/header.js
+++ pages/components/header.js
@@ -1,23 +1,18 @@
-import axios from "axios";
-import React from "react";
+import useSWR from 'swr'
 import Image from 'next/image'
 
-const baseURL = "http://frontity.test//wp-json/acf/v2/options";
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 
-export default function App() {
-  const [post, setPost] = React.useState(null);
+export default function Profile () {
+    const { data, error } = useSWR('http://frontity.test//wp-json/acf/v2/options', fetcher)
 
-  React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  if (!post) return null;
-
-  return (
-    <div>
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+    console.log({data});
+    // render data
+    return(
+<div>
         <nav className="navbar navbar-default scroll-to-fixed-fixed" id="menu"
         >
             <div className="container menuW">
@@ -31,7 +26,7 @@ export default function App() {
                     </button>
 
                     <a className="navbar-brand" href="#">
-                            <Image width="205" height="38" alt="img" src={post.acf.site_logo.url}></Image>
+                            <Image width="205" height="38" alt="img" src={data.acf.site_logo.url}></Image>
                     </a>
                 </div>
 
@@ -39,7 +34,7 @@ export default function App() {
                     <ul className="nav navbar-nav navbar-center">
                         <div className="menu-main-nav-container">
                         <ul id="primary-menu" className="menu">
-                        {post.acf.menu.map(book => (
+                        {data.acf.menu.map(book => (
                             <li key={book.link.title}  className="">
                                <a> {book.link.title}</a>
                             </li>
@@ -48,13 +43,12 @@ export default function App() {
                         </div>
                     </ul>
                     <div className="headerContact">
-                        <a href={`tel:${post.acf.phone_number.url}`} className="contactNumber"> {post.acf.phone_number.title} </a>
+                        <a href={`tel:${data.acf.phone_number.url}`} className="contactNumber"> {data.acf.phone_number.title} </a>
                     </div>
                 </div>
             </div>
         </nav>
     </div>
-)
-}
-
+    )
+  }
 
